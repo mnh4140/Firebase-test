78c4b939618713eb4bbbe94ecc75cbafc0e55da1
NH <mnh4140@gmail.com>
2025-05-31 14:52:13 +0900

    Initial Commit

---
+++ Firebase-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase-test
+//
+//  Created by NH on 5/31/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Firebase-testTests/Firebase_testTests.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_testTests.swift
+//  Firebase-testTests
+//
+//  Created by NH on 5/31/25.
+//
+
+import Testing
+@testable import Firebase_test
+
+struct Firebase_testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
